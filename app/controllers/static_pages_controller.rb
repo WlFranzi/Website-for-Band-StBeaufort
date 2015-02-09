3346239aad571f242e029210911a14913cf2bbb9
fwittleder <franziska.wittleder@gmail.com>
2015-02-09 16:47:07 +0100

    heroku mail

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,19 +1,16 @@
 class StaticPagesController < ApplicationController
-  def home
+ def home
   end
-end
 
  def music
   end
-end
 
  def contact
   end
-end
 
 def thank_you
   @name = params[:name]
   @email = params[:email]
   @message = params[:message]
   UserMailer.contact_form(@email, @name, @message).deliver
-end
+ end
--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,9 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
+  :user_name      => ENV['app33856905@heroku.com'],
+  :password       => ENV['7wu8ca9x'],
+  :domain         => 'stbeaufort.herokuapp.com',
   :enable_starttls_auto => true
 }
+
