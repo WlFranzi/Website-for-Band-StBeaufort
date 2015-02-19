ca4afbc71c4003df019cd9d6889cff1aad4d30bf
fwittleder <franziska.wittleder@gmail.com>
2015-02-19 17:01:40 +0100

    user mailer change

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,12 @@
-class UserMailer < ActionMailer::Base
-  default from: "franziska.wittleder@gmail.com"
+ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver
 
 
-
-
- def contact_form(email, name, message)
-	@message = message
-  	mail(:from => email, 
-	  		:to => 'franziska.wittleder@gmail.com', 
-	  		:subject => "A new contact form message from #{name}")
-	end
+ def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email, 
+	  	:to => 'franziska.wittleder@gmail.com', 
+	  	:subject => "A new contact form message from #{@name}", 
+	  	:body => @message).deliver
 end
