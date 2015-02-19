dfd5c4de159eb1d68eb0c5e0cd9cab7f586388e3
fwittleder <franziska.wittleder@gmail.com>
2015-02-19 20:09:34 +0100

    js links to javascripts

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,11 @@
-ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver
+class UserMailer < ActionMailer::Base
+  default from: "franziska.wittleder@gmail.com"
+end
 
-
- def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  ActionMailer::Base.mail(:from => @email, 
-	  	:to => 'franziska.wittleder@gmail.com', 
-	  	:subject => "A new contact form message from #{@name}", 
-	  	:body => @message).deliver
+  def contact_form(email, name, message)
+	@message = message
+  	mail(:from => email, 
+	  		:to => 'franziska.wittleder@gmail.com', 
+	  		:subject => "A new contact form message from #{name}")
+  end
 end
