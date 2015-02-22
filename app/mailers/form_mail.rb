6c21f0f49f2c3db15059939fc912b67340df15af
fwittleder <franziska.wittleder@gmail.com>
2015-02-22 11:58:47 +0100

    port to 465

--- app/mailers/form_mail.rb
+++ app/mailers/form_mail.rb
@@ -1,6 +1,5 @@
 ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'franzisksa.wittleder@gmail.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver
 
-
 def thank_you
   @name = params[:name]
   @email = params[:email]
