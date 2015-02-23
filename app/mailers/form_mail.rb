94ef13a55a058b562d961ee5e9ad188e7edd86a1
fwittleder <franziska.wittleder@gmail.com>
2015-02-23 19:16:00 +0100

    messagemailer add

--- app/mailers/form_mail.rb
+++
@@ -1,8 +0,0 @@
-ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'franzisksa.wittleder@gmail.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver
-
-def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  UserMailer.contact_form(@email, @name, @message).deliver
-end
---
+++ app/mailers/message_mailer.rb
@@ -0,0 +1,3 @@
+class MessageMailer < ActionMailer::Base
+end
+
