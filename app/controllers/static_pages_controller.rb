bea7161c0996b358f0133f9f9f638123c6c5831c
fwittleder <franziska.wittleder@gmail.com>
2015-02-23 20:05:57 +0100

    controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,5 +9,15 @@ class StaticPagesController < ApplicationController
  def contact
  end
 
+ def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email, 
+	  	:to => 'franziska.wittleder@gmail.com', 
+	  	:subject => "Huhu! A new contact form message from #{@name}", 
+	  	:body => @message).deliver
+ end
+
 end
 
