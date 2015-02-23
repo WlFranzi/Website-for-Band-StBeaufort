72b53af2a540a1751d22897a587f76774f5384c1
fwittleder <franziska.wittleder@gmail.com>
2015-02-23 12:33:37 +0100

    device mailers environment

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+config.action_mailer.default_url_options = { :host => 'stbeaufort.herokuapp.com' }
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
